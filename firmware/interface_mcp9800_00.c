0ef1288427083e1b7bc328a5ed6a2e63d1a639b5
Raphaël Assénat <raph@raphnet.net>
2010-07-10 18:04:38 +0000

    support for the mcp9800 at adress 0

---
+++ firmware/interface_mcp9800_00.c
@@ -0,0 +1,34 @@
+#include "interface.h"
+#include "usbtenki_cmds.h"
+#include "i2c.h"
+#include "mcp9800.h"
+
+#define MCP9800_ADDR    (MCP9800_ADDR_BASE + 0)
+
+int sensors_init(void)
+{
+	i2c_init(I2C_FLAG_INTERNAL_PULLUP);
+
+	return mcp9800_configure(MCP9800_ADDR, MCP9800_CFG_12BITS);
+}
+
+int sensors_getNumChannels(void)
+{
+	return 1;
+}
+
+int sensors_getChipID(unsigned char id)
+{
+	return USBTENKI_CHIP_MCP9800;
+}
+
+int sensors_getRaw(unsigned char id, unsigned char *dst)
+{
+	int res;
+	res = mcp9800_readRegister(MCP9800_ADDR, MCP9800_REG_TEMP, dst, 2);
+	if (res)
+		return res;
+	return 2;
+}
+
+
