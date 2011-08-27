03d543ff7a082aa876459e48e32d47f77cb6f82c
Raphaël Assénat <raph@raphnet.net>
2011-08-27 21:23:27 +0000

    16x gain switching and support

--- firmware/interface_tsl2561.c
+++ firmware/interface_tsl2561.c
@@ -3,6 +3,8 @@
 #include "i2c.h"
 #include "tsl2561.h"
 
+#include <util/delay.h>
+
 /* USBTenki head PCB connects address select to GND */
 #define TSL2561_ADDR    (TSL2561_ADDRESS_GND)
 
@@ -11,7 +13,7 @@ int sensors_init(void)
 	unsigned char tmp;
 	int res;
 	
-	i2c_init(I2C_FLAG_EXTERNAL_PULLUP);
+	i2c_init(I2C_FLAG_EXTERNAL_PULLUP, 55);
 
 	/* Powerup the chip */
 	tmp = 0x03;
@@ -20,15 +22,13 @@ int sensors_init(void)
 	if (res)
 		return res;
 
-return 0;
 	/* Setup gain and timing */
-//	tmp = 0x01;	// Integration time nominal 101ms
 	tmp = 0x02;	// Integration time nominal 402ms
-//	tmp |= 0x80;	// 16x gain // TODO: Additional channels?
 	res = tsl2561_writeRegister(TSL2561_ADDR, 
 				TSL2561_REG_TIMING | TSL2561_CMD_CMD, &tmp, 1);
 	if (res)
 		return res;
+
 	tmp = 0x00;	// No interrupts (0x00 is default value)
 	res = tsl2561_writeRegister(TSL2561_ADDR, 
 				TSL2561_REG_TIMING | TSL2561_CMD_CMD, &tmp, 1);
@@ -39,7 +39,7 @@ return 0;
 
 int sensors_getNumChannels(void)
 {
-	return 2;
+	return 4;
 }
 
 int sensors_getChipID(unsigned char id)
@@ -50,27 +50,71 @@ int sensors_getChipID(unsigned char id)
 			return USBTENKI_CHIP_TSL2561_IR_VISIBLE;
 		case 1:
 			return USBTENKI_CHIP_TSL2561_IR;
+		case 2:
+			return USBTENKI_CHIP_TSL2561_IR_VISIBLE_16X;
+		case 3:
+			return USBTENKI_CHIP_TSL2561_IR_16X;
 	}
 	return USBTENKI_CHIP_NONE;
 }
 
+static int switchGain(int gain_16x)
+{
+	static char last_16x = 0;
+	unsigned char tmp;
+	int res;
+
+	if (last_16x != gain_16x) {
+		tmp = 0x02;	// Integration time nominal 402ms
+		if (gain_16x)
+			tmp |= 0x10;	// 16x gain
+		res = tsl2561_writeRegister(TSL2561_ADDR, 
+				TSL2561_REG_TIMING | TSL2561_CMD_CMD, &tmp, 1);
+		if (res)
+			return res;
+		
+		_delay_ms(600);		
+	}
+
+	last_16x = gain_16x;
+
+	return 0;
+}
+
 int sensors_getRaw(unsigned char id, unsigned char *dst)
 {
+	int res;
+
 	dst[0] = 0x00;
 	dst[1] = 0x00;
 
 	switch(id)
 	{
 		case 0:
-			tsl2561_readRegister(TSL2561_ADDR, 
+			res = switchGain(0);
+			res = tsl2561_readRegister(TSL2561_ADDR, 
 					TSL2561_CMD_CMD | TSL2561_CMD_WORD | TSL2561_REG_DATA0_L,
 					dst, 2);
 			break;
 		case 1:
-			tsl2561_readRegister(TSL2561_ADDR, 
+			res = switchGain(0);
+			res = tsl2561_readRegister(TSL2561_ADDR, 
+				 	TSL2561_CMD_CMD | TSL2561_CMD_WORD | TSL2561_REG_DATA1_L,
+					dst, 2);
+			break;
+		case 2:
+			res = switchGain(1);
+			res = tsl2561_readRegister(TSL2561_ADDR, 
+					TSL2561_CMD_CMD | TSL2561_CMD_WORD | TSL2561_REG_DATA0_L,
+					dst, 2);
+			break;
+		case 3:
+			res = switchGain(1);
+			res = tsl2561_readRegister(TSL2561_ADDR, 
 					TSL2561_CMD_CMD | TSL2561_CMD_WORD | TSL2561_REG_DATA1_L,
 					dst, 2);
 			break;
+
 	}
 
 	return 2;
