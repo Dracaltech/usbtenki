826090d2d8c1d8c51346b1b0791bdd0fff0a916b
Raphaël Assénat <raph@raphnet.net>
2011-12-27 17:37:02 +0000

    global version header

---
+++ common/usbtenki_version.h
@@ -0,0 +1,12 @@
+#ifndef _usbtenki_version_h__
+#define _usbtenki_version_h__
+
+
+#define USBTENKI_VERSION	"1.8"
+#define USBTENKI_VERSION_MAJOR	1
+#define USBTENKI_VERSION_MINOR	8
+
+#define USBTENKI_COPYRIGHTS			"(C) Copyright 2007-2011 Raphaël Assénat <raph@raphnet.net>"
+#define USBTENKI_COPYRIGHTS_HTML	"(C) Copyright 2007-2011 Rapha&euml;l Ass&eacute;nat &lt;raph@raphnet.net&gt;"
+
+#endif // _usbtenki_version_h__
