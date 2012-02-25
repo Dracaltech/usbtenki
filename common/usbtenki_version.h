8fc8021fb90f4d1d8018e0e9b31b456defdbab9b
Raphaël Assénat <raph@raphnet.net>
2012-02-25 16:52:22 +0000

    New defines for the windows RC file

--- common/usbtenki_version.h
+++ common/usbtenki_version.h
@@ -3,9 +3,12 @@
 
 
 #define USBTENKI_VERSION	"1.9.2"
+#define USBTENKI_VERSION_STR0	"1.9.2\0"
 #define USBTENKI_VERSION_MAJOR	1
 #define USBTENKI_VERSION_MINOR	9
+#define USBTENKI_VERSION_NUM	1,9,2,0
 
+#define USBTENKI_COPYRIGHT_QTENKI	"(C) Copyright 2012 Raphaël Assénat"
 #define USBTENKI_COPYRIGHTS			"(C) Copyright 2007-2012 Raphaël Assénat <raph@raphnet.net>"
 #define USBTENKI_COPYRIGHTS_HTML	"(C) Copyright 2007-2012 Rapha&euml;l Ass&eacute;nat &lt;raph@raphnet.net&gt;"
 
