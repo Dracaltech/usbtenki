d97ae3e0c0ea3b045e4defc227ae565bfde89375
Raphaël Assénat <raph@raphnet.net>
2012-06-07 01:30:18 +0000

    New version PT100 RTD support New calibration data feature

--- common/usbtenki_cmds.h
+++ common/usbtenki_cmds.h
@@ -31,6 +31,18 @@
  */
 #define USBTENKI_GET_RAW			0x10
 
+/* Get calibration data for channel.
+ * 	request: USBTENKI_GET_CALIBRATION
+ * 	value: Channel ID
+ * 
+ * Returned data on error:
+ * [-1]
+ *
+ * Returned data:
+ * [0x10] [n bytes] [lrc]
+ */
+#define USBTENKI_GET_CALIBRATION	0x11
+
 /* Set the serial number characters one by one.
  *   request: USBTENKI_SET_SERIAL
  *   value: Low byte is character index. 
@@ -63,6 +75,19 @@
  * [0xf2] [lrc]
  */
 #define USBTENKI_SET_ADC_REF		0xf2
+
+
+/* Set the RTD current source calibration
+ *
+ *   request: USBTENKI_SET_RTD_CORR
+ *   value: percent * 100 (one signed byte)
+ *
+ * Returned data:
+ * [0xf2] [lrc]
+ */
+#define USBTENKI_SET_RTD_CORR		0xf3
+
+
 
 #define USBTENKI_CHIP_MCP9800	0x00
 #define USBTENKI_CHIP_LM75		0x01
