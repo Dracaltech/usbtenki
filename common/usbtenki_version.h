25b75084ce0846cb4b179b22d4c5ea349b8c76c3
Raphaël Assénat <raph@raphnet.net>
2013-01-22 17:57:49 +0000

    2013 copyright updates

--- common/usbtenki_version.h
+++ common/usbtenki_version.h
@@ -8,8 +8,8 @@
 #define USBTENKI_VERSION_MINOR	9
 #define USBTENKI_VERSION_NUM	1,9,7,0
 
-#define USBTENKI_COPYRIGHT_QTENKI	"(C) Copyright 2012 Raphaël Assénat"
-#define USBTENKI_COPYRIGHTS			"(C) Copyright 2007-2012 Raphaël Assénat <raph@raphnet.net>"
-#define USBTENKI_COPYRIGHTS_HTML	"(C) Copyright 2007-2012 Rapha&euml;l Ass&eacute;nat &lt;raph@raphnet.net&gt;"
+#define USBTENKI_COPYRIGHT_QTENKI	"(C) Copyright 2013 Raphaël Assénat"
+#define USBTENKI_COPYRIGHTS			"(C) Copyright 2007-2013 Raphaël Assénat <raph@raphnet.net>"
+#define USBTENKI_COPYRIGHTS_HTML	"(C) Copyright 2007-2013 Rapha&euml;l Ass&eacute;nat &lt;raph@raphnet.net&gt;"
 
 #endif // _usbtenki_version_h__
