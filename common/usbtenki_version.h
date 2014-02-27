1f7b24ba74215814efb920365e5df13e89e0e99c
Raphaël Assénat <raph@raphnet.net>
2014-02-27 02:57:33 +0000

    Changelog entries Version planned: 2.1.2

--- common/usbtenki_version.h
+++ common/usbtenki_version.h
@@ -2,14 +2,14 @@
 #define _usbtenki_version_h__
 
 
-#define USBTENKI_VERSION	"2.1.1"
-#define USBTENKI_VERSION_STR0	"2.1.1\0"
+#define USBTENKI_VERSION	"2.1.2"
+#define USBTENKI_VERSION_STR0	"2.1.2\0"
 #define USBTENKI_VERSION_MAJOR	2
 #define USBTENKI_VERSION_MINOR	1
 #define USBTENKI_VERSION_NUM	2,1,1,0
 
-#define USBTENKI_COPYRIGHT_QTENKI	"(C) Copyright 2013 Raphaël Assénat"
-#define USBTENKI_COPYRIGHTS			"(C) Copyright 2007-2013 Raphaël Assénat <raph@raphnet.net>"
-#define USBTENKI_COPYRIGHTS_HTML	"(C) Copyright 2007-2013 Rapha&euml;l Ass&eacute;nat &lt;raph@raphnet.net&gt;"
+#define USBTENKI_COPYRIGHT_QTENKI	"(C) Copyright 2013-2014 Raphaël Assénat"
+#define USBTENKI_COPYRIGHTS			"(C) Copyright 2007-2014 Raphaël Assénat <raph@raphnet.net>"
+#define USBTENKI_COPYRIGHTS_HTML	"(C) Copyright 2007-2014 Rapha&euml;l Ass&eacute;nat &lt;raph@raphnet.net&gt;"
 
 #endif // _usbtenki_version_h__
