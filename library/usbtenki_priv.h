d6357abc74eea5ac1cb7d01b3d0798d7b9e1d199
Raphael Assenat <raph@raphnet.net>
2017-03-19 11:51:44 -0400

    Remove unnecessary ifdef for windows

--- library/usbtenki_priv.h
+++ library/usbtenki_priv.h
@@ -1,12 +1,7 @@
 #ifndef _usbtenki_priv_h__
 #define _usbtenki_priv_h__
 
-
-#ifdef WINDOWS_VERSION
-#include "lusb0_usb.h"
-#else
 #include <usb.h>
-#endif
 
 struct USBTenki_list_ctx {
 	struct usb_bus *bus;
