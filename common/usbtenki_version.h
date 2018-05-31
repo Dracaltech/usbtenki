0a36afc17ded0c45884ffab7bac814d36e530578
Raphael Assenat <raph@raphnet.net>
2018-05-31 15:11:23 -0400

    Add copyright for win32 executable

--- common/usbtenki_version.h
+++ common/usbtenki_version.h
@@ -7,6 +7,7 @@
 #define USBTENKI_VERSION_MINOR	1
 #define USBTENKI_VERSION_NUM	2,1,14,0
 
+#define USBTENKI_COPYRIGHT_QTENKI   "(C) Copyright 2018 Dracal technologies inc."
 #define USBTENKI_COPYRIGHTS			"(C) Copyright 2007-2018 Raphaël Assénat, (C) Copyright 2018 Dracal technologies inc."
 #define USBTENKI_COPYRIGHTS_HTML	"(C) Copyright 2018 Dracal technologies inc.<br>(C) Copyright 2007-2018 Rapha&euml;l Ass&eacute;nat &lt;raph@raphnet.net&gt;"
 
