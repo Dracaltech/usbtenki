15c466f39dd4648d0b701488631a007e885ca5d0
Raphael Assenat <raph@raphnet.net>
2019-01-25 13:16:15 -0500

    Update version (2.1.15) and copyright years

--- common/usbtenki_version.h
+++ common/usbtenki_version.h
@@ -1,14 +1,14 @@
 #ifndef _usbtenki_version_h__
 #define _usbtenki_version_h__
 
-#define USBTENKI_VERSION	"2.1.14"
-#define USBTENKI_VERSION_STR0	"2.1.14\0"
+#define USBTENKI_VERSION	"2.1.15"
+#define USBTENKI_VERSION_STR0	"2.1.15\0"
 #define USBTENKI_VERSION_MAJOR	2
 #define USBTENKI_VERSION_MINOR	1
-#define USBTENKI_VERSION_NUM	2,1,14,0
+#define USBTENKI_VERSION_NUM	2,1,15,0
 
-#define USBTENKI_COPYRIGHT_QTENKI   "(C) Copyright 2018 Dracal technologies inc."
-#define USBTENKI_COPYRIGHTS			"(C) Copyright 2007-2018 Raphaël Assénat, (C) Copyright 2018 Dracal technologies inc."
-#define USBTENKI_COPYRIGHTS_HTML	"(C) Copyright 2018 Dracal technologies inc.<br>(C) Copyright 2007-2018 Rapha&euml;l Ass&eacute;nat &lt;raph@raphnet.net&gt;"
+#define USBTENKI_COPYRIGHT_QTENKI   "(C) Copyright 2018-2019 Dracal technologies inc."
+#define USBTENKI_COPYRIGHTS			"(C) Copyright 2007-2018 Raphaël Assénat, (C) Copyright 2018-2019 Dracal technologies inc."
+#define USBTENKI_COPYRIGHTS_HTML	"(C) Copyright 2018-2019 Dracal technologies inc.<br>(C) Copyright 2007-2018 Rapha&euml;l Ass&eacute;nat"
 
 #endif // _usbtenki_version_h__
